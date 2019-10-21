be08098b314fedee6ca0741aef8851797668ff8a
karkanis <carcani.patricio@gmail.com>
2019-10-20 23:29:05 -0300

    Anadido metodo PUT

--- src/main/java/ar/com/karkanis/api/controller/AlbumsController.java
+++ src/main/java/ar/com/karkanis/api/controller/AlbumsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,13 @@ public class AlbumsController {
 	
 	@PostMapping("/albums")
 	public Album guardarAlbum(@RequestBody Album album) {
+		serviceAlbums.guardar(album);
+		return album;
+	}
+	
+	//incluye el campo ID, por lo que JPA realiza un UPDATE en la bd
+	@PutMapping("/albums")
+	public Album modificar(@RequestBody Album album) {
 		serviceAlbums.guardar(album);
 		return album;
 	}
